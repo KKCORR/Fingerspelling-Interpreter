178da50ac224d90031a9c793f4c01f02543f5147
Rock <nattanon.hu@gmail.com>
2021-05-16 20:23:16 +0700

    group consecutive landmarks

--- src/app/hand_thread.py
+++ src/app/hand_thread.py
@@ -7,13 +7,19 @@ import time
 
 
 class HandThread(StoppableThread):
-    def __init__(self, application=None):
+    def __init__(self, application=None, acceptable_dist=0.20, consecutive_seq=20):
         super().__init__()
         self.application = application
         self.hand_landmarker = HandLandmark()
         self.char_predictor = CharPredictor(application.xgb_model)
+        self.acceptable_dist = acceptable_dist
+        self.consecutive_seq = consecutive_seq
+        self.consecutive_stable = 0
+        self.prev_landmarks = None
 
     def run(self):
+        self.consecutive_stable = 0
+        self.prev_landmarks = None
         while not self.stopped():
             if self.application.is_record:
                 frame = self.application.frame
@@ -23,4 +29,26 @@ class HandThread(StoppableThread):
                 frameRGB.flags.writeable = True
                 self.application.landmarks = landmarks
                 if landmarks:
-                    print(self.char_predictor.best_predict(landmarks[0]))
+                    if self.prev_landmarks:
+                        dist = 0
+                        for lm, plm in zip(landmarks[0].landmark, self.prev_landmarks[0].landmark):
+                            dist = (lm.x - plm.x)**2 + (
+                                lm.y - plm.y)**2 + (lm.z - plm.z)**2
+                        dist = dist**0.5
+
+                        if dist < self.acceptable_dist:
+                            self.consecutive_stable += 1
+                        else:
+                            self.consecutive_stable = 0
+
+                        if self.consecutive_stable == self.consecutive_seq:
+                            self.consecutive_stable = 0
+                            char, _ = self.char_predictor.best_predict(
+                                landmarks[0])
+                            print(char)
+
+                        self.prev_landmarks = landmarks
+                    else:
+                        self.prev_landmarks = landmarks
+                else:
+                    self.consecutive_stable = 0
