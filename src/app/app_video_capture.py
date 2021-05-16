b1cabb0d7bf6d5dab00a640db0d566aebfda675f
Rock <nattanon.hu@gmail.com>
2021-05-16 10:51:04 +0700

    add AppVideoCapture class

---
+++ src/app/app_video_capture.py
@@ -0,0 +1,44 @@
+import cv2
+
+
+class AppVideoCapture:
+    """
+    A customize video capture class based on cv2.VideoCapture
+
+    Attributes
+    ----------
+    vid: cv2.VideoCapture
+        VideoCapture object
+    width: int
+        width of the video frame
+    height: int
+        height of the video frame
+    """
+
+    def __init__(self, video_source=0):
+        # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+
+        # Get video source width and height
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    def get_frame(self):
+        """
+        Return video frame BGR (blue-green-red) format
+        """
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
+
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
